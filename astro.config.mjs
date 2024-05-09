ac080241172a47b8abf92d01fc3ac480686ce6a1
Brando Vidal <yisusdracon@gmail.com>
2024-05-08 21:20:05 -0500

    chore: use react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
-
 import db from "@astrojs/db";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [db()]
+  integrations: [db(), react()]
 });
