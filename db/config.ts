b93151ed6792917daa4d672b51d91a0d2576eb4d
Brando Vidal <yisusdracon@gmail.com>
2024-05-08 19:38:47 -0500

    chore: implement astro db

---
+++ db/config.ts
@@ -0,0 +1,29 @@
+// https://astro.build/db/config
+import { column, defineDb, defineTable } from 'astro:db'
+
+const User = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text(),
+    email: column.text()
+  }
+})
+
+const Link = defineTable({
+  columns: {
+    url: column.text(),
+    slug: column.text({ unique: true }),
+    userId: column.number({
+      optional: true,
+      references: () => User.columns.id
+    })
+  }
+})
+
+// https://astro.build/db/config
+export default defineDb({
+  tables: {
+    User,
+    Link
+  }
+})
