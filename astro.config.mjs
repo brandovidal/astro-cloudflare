464c50f966b8ed0d622a43292ad1d095bacf6f56
Brando Vidal <yisusdracon@gmail.com>
2024-05-08 21:21:58 -0500

    chore: use tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 import db from "@astrojs/db";
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [db(), react()]
+  integrations: [db(), react(), tailwind()]
 });
